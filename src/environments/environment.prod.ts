256f5fbec09255afe1576e99fdbbff8b7faa8d3d
ShafayetMahmudArco <arco.smahmud@gmail.com>
2025-10-12 01:54:56 +0600

    environment modification

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,11 @@
-export const environment = {
+export const environmentOld = {
   production: true,
   // apiBaseUrl: window.location.protocol + '//' + window.location.host + '/api',  // Dynamic based on current domain
   apiBaseUrl: 'https://mudhammataan-api.azurewebsites.net/api',
   allowedFrontendDomains: ['*']
+};
+
+export const environment = {
+  production: true,
+  apiBaseUrl: window.location.protocol + '//' + window.location.host + '/api'
 };
