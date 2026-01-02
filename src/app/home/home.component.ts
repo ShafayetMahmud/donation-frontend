9a04704c9c88f6c7ef887041d413dfa1a8c9c5fa
raselmushfiq111 <rasel.mushfiq111@gmail.com>
2026-01-02 17:51:42 +0600

    homepage translation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,19 +5,24 @@ import { MatButtonModule } from '@angular/material/button';
 import { SubdomainService, Campaign } from '../services/subdomain.service';
 import { CommonModule, AsyncPipe } from '@angular/common';
 import { Observable } from 'rxjs';
+import { LanguageService } from '../services/language.service';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, CommonModule, AsyncPipe],
+  imports: [MatToolbarModule, MatButtonModule, CommonModule, AsyncPipe, TranslateModule],
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-
   public campaign$: Observable<Campaign | null>;
 
-  constructor(public subdomainService: SubdomainService, private router: Router) {
+  constructor(
+    public subdomainService: SubdomainService,
+    private router: Router,
+    public langService: LanguageService
+  ) {
     this.campaign$ = this.subdomainService.campaign$;
   }
 
