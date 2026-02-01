2427c704b610eff92d3005caf4488471c4f5730d
ShafayetMahmudArco <arco.smahmud@gmail.com>
2026-02-01 11:12:06 +0600

    popup fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,15 +1,15 @@
 export const environmentOld = {
   production: true,
   // apiBaseUrl: window.location.protocol + '//' + window.location.host + '/api',  // Dynamic based on current domain
-  apiBaseUrl: 'https://mudhammataan-api.azurewebsites.net/api',
+  apiBaseUrl: window.location.protocol + '//' + window.location.host + '/api',
   allowedFrontendDomains: ['*'],
   msalConfig: {
     auth: {
       // clientId: '99d94324-a3a8-4ace-b4b2-0ae093229b62',
       clientId: '9a7bbbc7-dad0-4ef7-841b-b8b45a7605ed',
       authority: 'https://login.microsoftonline.com/8adf6212-f010-45db-b70e-7bc732eb2759', // tenant ID
-      redirectUri: 'https://mudhammataan.com', // Angular base URL
-      postLogoutRedirectUri: 'https://mudhammataan.com'
+      redirectUri: 'window.location.origin', // Angular base URL
+      postLogoutRedirectUri: 'window.location.origin'
     },
     cache: {
       cacheLocation: 'localStorage',
@@ -32,8 +32,8 @@ export const environment = {
       // clientId: '99d94324-a3a8-4ace-b4b2-0ae093229b62',
       clientId: '9a7bbbc7-dad0-4ef7-841b-b8b45a7605ed',
       authority: 'https://login.microsoftonline.com/8adf6212-f010-45db-b70e-7bc732eb2759', // tenant ID
-      redirectUri: 'https://mudhammataan.com', // Angular base URL
-      postLogoutRedirectUri: 'https://mudhammataan.com'
+      redirectUri: 'window.location.origin', // Angular base URL
+      postLogoutRedirectUri: 'window.location.origin'
     },
     cache: {
       cacheLocation: 'localStorage',
