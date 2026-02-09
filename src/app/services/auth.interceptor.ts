35c541bcde0c63070265a3804beff9a0e596a4b9
ShafayetMahmudArco <arco.smahmud@gmail.com>
2026-02-09 15:14:31 +0600

    campaign update for subdomain

--- src/app/services/auth.interceptor.ts
+++ src/app/services/auth.interceptor.ts
@@ -3,6 +3,7 @@ import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/c
 import { Observable, from } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
 import { AuthService } from './auth.service';
+import { environment } from '../../environments/environment';
 
 // @Injectable()
 // export class AuthInterceptor implements HttpInterceptor {
@@ -32,27 +33,59 @@ export class AuthInterceptor implements HttpInterceptor {
 
   constructor(private authService: AuthService) {}
 
+  //old
+
+  // intercept(req: HttpRequest<any>, next: HttpHandler) {
+
+  //   return from(this.authService.getAccessToken()).pipe(
+  //     switchMap(token => {
+
+  //       // If no token (anonymous), pass request through
+  //       if (!token) {
+  //         return next.handle(req);
+  //       }
+
+  //       const authReq = req.clone({
+  //         setHeaders: {
+  //           Authorization: `Bearer ${token}`
+  //         }
+  //       });
+
+  //       console.log('AuthInterceptor token:', token);
+
+  //       return next.handle(authReq);
+  //     })
+  //   );
+  // }
+
+  //old
+
+  //new
+
   intercept(req: HttpRequest<any>, next: HttpHandler) {
 
-    return from(this.authService.getAccessToken()).pipe(
-      switchMap(token => {
-
-        // If no token (anonymous), pass request through
-        if (!token) {
-          return next.handle(req);
-        }
-
-        const authReq = req.clone({
-          setHeaders: {
-            Authorization: `Bearer ${token}`
-          }
-        });
-
-        console.log('AuthInterceptor token:', token);
-
-        return next.handle(authReq);
-      })
-    );
+  // ⭐ ONLY attach token to your backend API
+  if (!req.url.includes(environment.apiBaseUrl)) {
+    return next.handle(req);
   }
+
+  return from(this.authService.getAccessToken()).pipe(
+    switchMap(token => {
+
+      if (!token) return next.handle(req);
+
+      const authReq = req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+
+      return next.handle(authReq);
+    })
+  );
+}
+
+
+  //new
 }
 
